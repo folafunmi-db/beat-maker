a083096cb561bb88890f0bbce029fc5051de2f0e
Folafunmi <folafunmimustapha@gmail.com>
2020-09-23 16:47:26 +0100

    Added jump animation

--- script.js
+++ script.js
@@ -3,12 +3,32 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sounds");
   const pads = document.querySelectorAll(".pads div");
-  console.log(sounds);
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#60d394",
+    "#b0d360",
+    "#6084d3",
+    "#ae60d3",
+    "#d37b60",
+    "#e44949",
+  ];
+  // console.log(sounds);
 
   // Playing the sounds
   pads.forEach((pad, index) => {
-    pad.addEventListener('click', function () {
+    pad.addEventListener("click", function () {
+      // Reset the current time to enable the sound to interrupt another
+      sounds[index].currentTime = 0;
       sounds[index].play();
+
+      createBubbles(index);
     });
   });
+  // Bubble function
+  const createBubbles = (index) => {
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = "jump 1s ease";
+  };
 });
