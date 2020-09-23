018f39770e342d56b214acaee493c3ab6b9f94c3
Folafunmi <folafunmimustapha@gmail.com>
2020-09-23 16:22:00 +0100

    Added click and play function the pads

--- script.js
+++ script.js
@@ -3,4 +3,12 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sounds");
   const pads = document.querySelectorAll(".pads div");
+  console.log(sounds);
+
+  // Playing the sounds
+  pads.forEach((pad, index) => {
+    pad.addEventListener('click', function () {
+      sounds[index].play();
+    });
+  });
 });
