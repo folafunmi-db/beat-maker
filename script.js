5b3bcaef8f9f885ee566446f29d720a6a1112b98
Folafunmi <folafunmimustapha@gmail.com>
2020-09-23 16:05:27 +0100

    Added javascript file

---
+++ script.js
@@ -0,0 +1,6 @@
+/*jshint esversion: 10 */
+
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sounds");
+  const pads = document.querySelectorAll(".pads div");
+});
